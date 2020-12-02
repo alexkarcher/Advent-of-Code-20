2dd938d6895f2d8d5e8f4782a7fa3c50b54c2c50
Alex Karcher <alexkarcher@users.noreply.github.com>
2020-12-02 08:18:31 -0800

    🎄Day 2 🎁

--- Day 2/Problem1.js
+++ Day 2/Problem1.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var array = fs.readFileSync('my-file.txt').toString().split("\n");
+console.log(array);
 var answers = [];
+
+var validpasswords = 0;
+
 for(i = 0; i < array.length; i++) {
-    for (j = i; j < array.length; j++){
-        if(+array[i]+ +array[j] == 2020){
-            console.log(+array[i]* +array[j]);
-        }
+    var policy = array[i].split(" ");
+
+    var range = policy[0].split("-");
+    var lower = range[0];
+    var upper = range[1];
+
+    var letter = policy[1].substring(0,1);
+
+    var password = policy[2];
+
+    var counter = 0
+    for(j = 0; j< password.length; j++){
+        if(password.substring(j,j+1) === letter)
+            counter++;
     }
+    if(counter>=lower && counter <= upper){
+        console.log("🎄🎄🎄"+password);
+        validpasswords++;
+    }
+        
 }
+
+console.log("Valid Passwords: "+validpasswords);
--- Day 2/Problem2.js
+++ Day 2/Problem2.js
@@ -1,13 +1,30 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var array = fs.readFileSync('my-file.txt').toString().split("\n");
+//console.log(array);
 var answers = [];
+
+var validpasswords = 0;
+
 for(i = 0; i < array.length; i++) {
-    for (j = i; j < array.length; j++){
-        for(q = j; q < array.length; q++){
-            if(+array[i]+ +array[j] + +array[q] == 2020){
-                console.log(+array[i]* +array[j]* +array[q]);
-            }
-        }
+    var policy = array[i].split(" ");
+
+    var range = policy[0].split("-");
+    var lower = range[0];
+    var upper = range[1];
+
+    var letter = policy[1].substring(0,1);
+
+    var password = policy[2];
+
+    var first = password.substring(lower-1,lower) === letter;
+    var second = password.substring(upper-1,upper) === letter;
+    //console.log(first +" "+ second + password);
+    if((first && !second) || (!first && second)){
+        //console.log("🎄🎄🎄"+password);
+        validpasswords++;
     }
+        
 }
+
+console.log("Valid Passwords: "+validpasswords);
