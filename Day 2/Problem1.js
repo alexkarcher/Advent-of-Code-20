13a5957f1b5ddbbd4d5a43fa22d171ee2bc7365e
Alex Karcher <alexkarcher@users.noreply.github.com>
2020-12-01 18:37:16 -0800

    Added Day 2 🤫

--- Day 2/Problem1.js
+++ Day 2/Problem1.js
@@ -4,10 +4,8 @@ var array = fs.readFileSync('my-file.txt').toString().split("\n");
 var answers = [];
 for(i = 0; i < array.length; i++) {
     for (j = i; j < array.length; j++){
-        for(q = j; q < array.length; q++){
-            if(+array[i]+ +array[j] + +array[q] == 2020){
-                console.log(+array[i]* +array[j]* +array[q]);
-            }
+        if(+array[i]+ +array[j] == 2020){
+            console.log(+array[i]* +array[j]);
         }
     }
 }
