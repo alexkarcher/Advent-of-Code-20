47cf1336935c598251cdc597a090670e0f1d3d12
Alex Karcher <alexkarcher@users.noreply.github.com>
2020-12-15 08:17:35 -0800

    First attempt at day 7

--- Day 7/Problem1.js
+++ Day 7/Problem1.js
@@ -1,32 +1,79 @@
 #!/usr/bin/env node
-function countAnswers(answers){
-    var objects = 0;
-    for(i in answers){
-        objects++;
+function removeBag(color){
+    var cleanString = new String;
+    var nospace = color.split(" ");
+    for(i in nospace){
+        if(!nospace[i].match(/bag|bags/) && i>0) cleanString+=" ";
+        if(!nospace[i].match(/bag|bags/)) cleanString+=nospace[i];   
     }
-    return objects;
+    return cleanString;
 }
-var fs = require('fs');
-var array = fs.readFileSync('my-file.txt').toString().split("\n");
-//console.log(array);
 
-var answers = {};
-
-var count = 0;
-
-for(i in array) {
-    //console.log("Line: "+array[i]);
-    if(array[i] === ""){
-        console.log(answers);
-        count += countAnswers(answers);
-        answers = {};
+function extractNumber(rule){
+    rule = removeBag(rule);
+    var nospace=rule.split(" ");
+    var r = {}
+    r.count = nospace[0];
+    r.color = new String;
+    for(i in nospace){
+        if(i>1 && !nospace[i].match(/bag|bags/)) r.color+=" ";
+        if(i>0 && !nospace[i].match(/bag|bags/)) r.color+=nospace[i];
     }
-    else{
-        for(j in array[i]){
-            var question = array[i][j];
-            answers[question]=1;
+    return r;
+}
+
+function checkBag(i_outer,inner,rules,winner){
+    //console.log("checking bag "+outer);
+    var outer = i_outer;
+    if(winner.didWeWin){
+        //console.log("Winning Bag!!!")
+        return;
+    } 
+    for(i in outer){
+        if(outer[i] === "no other bags")return;
+
+        var color = extractNumber(outer[i]);
+        if(inner === color.color){
+            winner.didWeWin=true;
+            //console.log("Winner!!!!");
+            return;
+        }
+        else{
+            return checkBag(rules[color.color], inner, rules, winner);
         }
     }
+    
 }
-count += countAnswers(answers);
+var fs = require('fs');
+var array = fs.readFileSync('my-file.txt').toString().split(".\n");
+//console.log(array);
+
+var winningBag = "shiny gold";
+
+var count = 0;
+var rules = [];
+
+//Load in rules
+for(i in array) {
+    var line = array[i].split(" contain ");
+    var outer = removeBag(line[0]);
+    var inners = line[1].split(", ");
+    rules[outer]=inners;
+}
+
+console.log(rules.length);
+//Check if you can have other bags
+for(i in rules){
+    //console.log("i:"+i)
+    var winner = { didWeWin:false };
+    var tempbag = rules[i];
+    var tempwinner = i;
+    checkBag(tempbag,winningBag,rules,winner);
+    
+    if(winner.didWeWin && tempwinner!==winningBag){ 
+        count++;
+        console.log("winner " + tempwinner);
+    }
+}
+
 console.log("🎄🎫🎄: "+count);
